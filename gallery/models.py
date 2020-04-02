d946e3796e8c730ebfc474b9b2eb37e1251d54e3
Megatron <beforebook@hotmail.com>
2020-04-02 21:39:05 +0800

    1

--- gallery/models.py
+++ gallery/models.py
@@ -3,5 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Gallery(models.Model):
-    description = models.CharField(max_length=80)
+    title = models.CharField(default='请输入作品标题', max_length=30)
+    description = models.CharField(default='在这里写作品的简介', max_length=80)
+    image = models.ImageField(default='default.png', upload_to='images/')
 
+
+    def __str__(self):
+        return self.title
