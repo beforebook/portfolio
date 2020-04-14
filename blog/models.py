fe2db1b579ef776dea3d33e6393cadee9af2a458
Megatron <beforebook@hotmail.com>
2020-04-14 19:21:53 +0800

    增加功能

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,10 @@ class Blog(models.Model):
     title = models.CharField(default='请输入博客标题', max_length=50)
     date = models.DateField()
     image = models.ImageField(default='default.png', upload_to='images/')
-    text = models.TextField(default='请输入正文', max_length=500)
+    text = models.TextField(default='请输入正文', max_length=2500)
 
     def __str__(self):
         return self.title
+
+    def short_text(self):
+        return self.text[:100] + '...'
