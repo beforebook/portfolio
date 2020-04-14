a2a5801ec0fda5d0686a0b8c650326a95d808f04
Megatron <beforebook@hotmail.com>
2020-04-14 18:31:19 +0800

    增加blog

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Blog(models.Model):
+
+    title = models.CharField(default='请输入博客标题', max_length=50)
+    date = models.DateField()
+    image = models.ImageField(default='default.png', upload_to='images/')
+    text = models.TextField(default='请输入正文', max_length=500)
+
+    def __str__(self):
+        return self.title
